b1eb9ce69a8dfbceb04a6ef3c4354fc83477f37f
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-15 11:43:38 +0530

    content update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 // function for check username and password and load sign in
 function validate() {
+  
+  // sign in user name pass (kasun, kasun)
   var username = document.getElementById("userName").value;
   var password = document.getElementById("password").value;
 
-  if (username == "kasun" && password == "kasun") {
+
+
+  if (username == "kasun" && password == "kasun12") {
     window.location = "welcome.html";
   } else {
     alert("invaild information");
--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -1,10 +1,19 @@
 // when press sign out redirect index.htl and signout
-function signOut(){
-    window.location = "index.html";
+function signOut() {
+  window.location = "index.html";
 }
 
-
 // add new user function
-function signUp(){
-   
+function signUp() {
+  // Get username and password input values
+  var newUsername = document.getElementById("newUserName").value;
+  var newUserPassword = document.getElementById("newUserpassword").value;
+
+  alert("User successfully created!");
+
+  console.log(newUsername);
+  console.log(newUserPassword);
+
+  document.getElementById("newUserName").value="";
+  document.getElementById("newUserpassword").value="";
 }
