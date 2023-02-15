5c9bfa674a66a38fd36f4d88e907708770179940
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-15 10:51:46 +0530

    add welcome page after sign in

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-// function for check username and password and load sign in page
+// function for check username and password and load sign in
 function validate() {
   var username = document.getElementById("userName").value;
   var password = document.getElementById("password").value;
 
   if (username == "kasun" && password == "kasun") {
-    window.location = "signin.html";
+    window.location = "welcome.html";
   } else {
     alert("invaild information");
   }
